a7db442664e7ca86445516aee368d26275a34d7c
Chen.Oppenhaim <12Meetup3>
2019-01-03 11:59:36 +0200

    #1 Fixed

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 module.exports = function isIsraeliIdValid(id)
 {
-    var strId = String(id).trim();
-    if (strId.length !== 9) {
+    let strId = String(id).trim();
+    if (strId.length > 9) {
         return false;
     }
-    var counter = 0, rawVal, actualVal;
-    for (var i = 0; i < strId.length; i++) {
+    if (strId.length < 9) {
+        while (strId.length < 9) strId = "0" + strId;
+    }
+    let counter = 0, rawVal, actualVal;
+    for (let i = 0; i < strId.length; i++) {
         rawVal = Number(strId[i]) * ((i % 2) + 1);
         actualVal = rawVal > 9 ? (rawVal - 9) : rawVal;
         counter += actualVal;
